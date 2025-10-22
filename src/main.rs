cbd2fa0836f7c3b6cd9e4306c7f568c8b3a6b614
Nick Ivanov <sm230@pm.me>
2025-10-22 07:41:42 -0400

    txformat ver. 1 implementation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 use actix_web::{ post, web, App, HttpServer, Result };
 use serde::{ Serialize, Deserialize };
 
-#[derive(Deserialize)]
+
+#[derive(Serialize, Deserialize, Debug)]
+struct St {
+    obj: String,
+    chash: String,
+    nvalue: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Txn {
+    from: String,
+    st: St,
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
 struct TxRequest {
-    jawn: String,
+    ver: u64,
+    txn: Txn,
+    sig: String,
 }
 
 async fn index(txrequest: web::Json<TxRequest>) -> Result<String> {
     println!("Index hit...");
-    Ok(format!("Your jawn: {}", txrequest.jawn))
+    Ok(format!("Your jawn: {}", txrequest.ver))
 }
 
 #[actix_web::main]
